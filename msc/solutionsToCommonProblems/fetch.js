c1895003e5c1118b5552dee16296ca7864acf459
issa-char <128964601+issa-char@users.noreply.github.com>
2024-02-19 18:45:05 +0000

    catching errors in asychronous js

--- msc/solutionsToCommonProblems/fetch.js
+++ msc/solutionsToCommonProblems/fetch.js
@@ -13,6 +13,8 @@ console.log("started request...");
 */
 
 // an elegant implementation of the above, just to avoid the callback hell
+
+/*
 const fetch_p = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
 
 console.log(fetch_p);
@@ -24,4 +26,23 @@ fetch_p
 });
 
 console.log("Started request...");
+*/
+
+// catching errors
+
+const fetchP = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.jso");
+
+fetchP
+	.then((response) => {
+		if (!response.ok){
+			throw new Error(`HTTP error: ${response.status}`);
+		}
+		return response.json();
+	})
+	.then((data) => {
+		console.log(data[0].name);
+	})
+	.catch((error) => {
+		console.log(`Could not get products: ${error}`);
+	});
 
